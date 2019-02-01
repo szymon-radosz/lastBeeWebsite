0797922858fdeceac0d1e8c6cae9db6dc31625d7
RadoszSzymon <radoszszymon@gmail.com>
2019-02-01 17:43:30 +0100

    add website provider from traditional login

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -66,6 +66,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'provider' => 'website',
             'password' => Hash::make($data['password']),
         ]);
     }
