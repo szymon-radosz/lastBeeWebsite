78794fc12a407b9224c61f58d9b04f4e8bfae4c7
RadoszSzymon <radoszszymon@gmail.com>
2019-02-06 18:51:52 +0100

    email template after fb register

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Mail\WelcomeMail;
+use Illuminate\Support\Facades\Mail;
 
 class LoginController extends Controller
 {
