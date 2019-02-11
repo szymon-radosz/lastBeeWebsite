bbc1078d287aa12382f1b0e21aef79612714bd94
RadoszSzymon <radoszszymon@gmail.com>
2019-02-11 17:15:34 +0100

    fixed WelcomeMailPL class name

--- app/Mail/WelcomeMailPL.php
+++ app/Mail/WelcomeMailPL.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class WelcomeMail extends Mailable
+class WelcomeMailPL extends Mailable
 {
     use Queueable, SerializesModels;
 
